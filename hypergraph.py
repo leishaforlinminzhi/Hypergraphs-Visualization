bbd15d6ac1997fa59ddeac374f6770374e1f3b31
leishaforlinminzhi <15611664060@qq.com>
2024-04-03 10:32:08 +0800

    debug: fixed the problem about index of points

--- hypergraph.py
+++ hypergraph.py
@@ -6,10 +6,11 @@ class Hypergraph:
             self.num_v = max(v_list) + 1
         else:
             self.num_v = 0
-        self.edges = e_list # 保存边的列表 其中元素为所有边[]
-        self.points = {} # 保存顶点的坐标
+        self.edges = e_list # 保存边的列表 其中元素为所有边[边中的点]
+        self.points = {} # 保存顶点i对应的坐标
         self.R = {} # 保存关系 R[i]={[..],[..],..} 其中每个元素为关联i个点的边 为e_list的一个划分
         self.d = {} # 顶点的度
+        self.v = [] # 保存顶点的序号
 
     def dataloader(self, filename: str):
         self.edges = []
@@ -27,11 +28,15 @@ class Hypergraph:
 
                 for v in edge:
                     if v not in self.d:
-                        self.d[v] = 0
+                        self.d[v] = 1
                     else:
                         self.d[v] += 1
 
         self.vertices = list(self.vertices)
         self.R = dict(sorted(self.R.items()))
         self.d = dict(sorted(self.d.items()))
+        self.v = list(self.d.keys())
 
+        print("R:",self.R)
+        print("d:",self.d)
+        print("v:",self.v)
